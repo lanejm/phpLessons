3c31d9f83b2e4fa3822cd9b2ca2afb0545361104
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2023-10-11 16:40:37 -0400

    loops

---
+++ loops.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<?php 
+	
+		//WHILE LOOP
+		$x = 1;
+		  
+		while($x < 10) {
+		   echo "The number is: $x <br>";
+		   $x++;
+		} 
+		echo "<hr />";
+		
+		//DO WHILE LOOP
+		//only repeat the loop if condition is true
+		$x = 6;		
+		do {
+		echo "The number is: $x <br>";
+		$x++;
+		} while ($x <= 5);
+		
+	?>   
+
+</body>
+</html>
